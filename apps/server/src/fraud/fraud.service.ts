7c46d178ec9b2b4d70d9ff15525937d05939bd5e
strangedev <strngecloud.iv@gmail.com>
2026-06-29 10:42:05 +0100

    fix: remove unused LessThan import from fraud service

--- apps/server/src/fraud/fraud.service.ts
+++ apps/server/src/fraud/fraud.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, LessThan, MoreThan } from 'typeorm';
+import { Repository, MoreThan } from 'typeorm';
 import { SpendEntity } from '../database/entities/spend.entity';
 import { RedisService } from '../redis/redis.service';
 import { FraudCheckException } from '../common/exceptions/custom.exceptions';
