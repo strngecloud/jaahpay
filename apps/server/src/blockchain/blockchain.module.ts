69af3bc8e6085ddab846339f423df30bf312463c
strangedev <strngecloud.iv@gmail.com>
2026-07-21 00:10:16 +0100

    chore: update blockchain module dependencies

--- apps/server/src/blockchain/blockchain.module.ts
+++ apps/server/src/blockchain/blockchain.module.ts
@@ -1,9 +1,10 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { BlockchainService } from './blockchain.service';
 import { SpendModule } from '../spend/spend.module';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [forwardRef(() => SpendModule)],
+  imports: [forwardRef(() => SpendModule), RedisModule],
   providers: [BlockchainService],
   exports: [BlockchainService],
 })
